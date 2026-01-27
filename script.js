2fa660055fe9413ecd5241ce85e8809eea80f2b6
Talha <175202306+kxllshot1@users.noreply.github.com>
2026-01-27 16:40:12 +0500

    styled the header and the nav, also implement functionality to open and close the nav

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 "use strict";
+
+const imgIcon = document.querySelector(".img-nav-icon");
+const iconHamburgerSrc = "images/bars-solid-full.svg";
+const iconCloseSrc = "images/x-solid-full.svg";
+const btnNav = document.querySelector(".btn-nav");
+const boxNav = document.querySelector(".box-nav-links");
+const imgNavHamurger = document.querySelector(".img-nav-icon");
+btnNav.addEventListener("click", function () {
+  boxNav.classList.toggle("box-nav-active");
+  imgIcon.getAttribute("src") === iconHamburgerSrc
+    ? imgIcon.setAttribute("src", iconCloseSrc)
+    : imgIcon.setAttribute("src", iconHamburgerSrc);
+});
